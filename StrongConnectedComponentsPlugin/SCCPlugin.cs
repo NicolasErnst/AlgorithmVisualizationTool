d2da23cdad4a97fcfef2f6131ad65d11455b5814
Nicolas Ernst <dev@nicolasernst.de>
2021-02-01 16:54:19 +0100

    Added project for strong connected components

---
+++ StrongConnectedComponentsPlugin/SCCPlugin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphAlgorithmPlugin;
+
+namespace StrongConnectedComponentsPlugin
+{
+    public class SCCPlugin : GraphAlgorithmPlugin<Vertex, Edge<Vertex>>
+    {
+        public override string AlgorithmName => "Strong Connected Components";
+
+        public override GraphDirectionType CompatibleGraphDirections => GraphDirectionType.Directed; 
+
+        protected override Task RunAlgorithm(Vertex startVertex)
+        {
+            // TODO
+        }
+    }
+}
+
